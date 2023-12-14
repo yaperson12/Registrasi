43b69e6033083b104b67b6a52a60c45234655b9c
yaperson12 <jjmulia@gmail.com>
2023-12-14 21:16:28 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/header.js';
+import RegistrationForm from './Components/registrationform.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
